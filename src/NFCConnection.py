58143bdbb8a3d1aecf87e810941aab446bc21391
Sherene Liu <jl205@rice.edu>
2024-09-20 22:47:29 -0500

    set up connection success

--- src/NFCConnection.py
+++ src/NFCConnection.py
@@ -1,3 +1,4 @@
+from smartcard.CardMonitoring import CardMonitor, CardObserver
 from smartcard.System import readers
 from smartcard.util import toHexString
 import sys
@@ -7,7 +8,7 @@ import time
 
 #print ("helloworld")
 class NFCconnection:
-    def _init_(self):
+    def __init__(self):
         self.reader = None
         self.connection = None
     #establish a connection with the nfc reader
@@ -18,18 +19,29 @@ class NFCconnection:
             sys.exit()
         #select the first reader
         self.reader = r[0]
-        print("Using reader: ", self.reader)
+        print(f"Using reader: {self.reader.__str__()}")
         #connect to the card
-        reading_loop = 1
+        reading_loop = True
         while(reading_loop):
             try:
                 self.connection = self.reader.createConnection()
                 self.connection.connect()
                 print("Successfully connected to nfc card!")
+                reading_loop = False
             except Exception as e:
-                print("No connection established. Try again.")
-            time.sleep(.2)
+                x = 0
+            time.sleep(0.2)
     def disconnect(self):
         if self.connection.isConnected():
             self.connection.disconnect()
             print("Connection closed.")
+def main():
+    waiting_for_card = True
+    while waiting_for_card:
+        print("Waiting for card tab....")
+        nfc_conn = NFCconnection()
+        nfc_conn.connect_reader()
+        time.sleep(1)
+    nfc_conn.disconnect()
+if __name__ == "__main__":
+    main()
