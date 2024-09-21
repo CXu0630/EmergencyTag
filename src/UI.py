db337ca36a170fefb385c16c59da7fe5569336de
Sherene Liu <jl205@rice.edu>
2024-09-21 14:48:08 -0500

    started on UI

---
+++ src/UI.py
@@ -0,0 +1,7 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QLabel, QComboBox, QLineEdit, QPushButton, QVBoxLayout, QWidget, QMessageBox
+from EmInfoLength import EmInfoLength
+
+class UserInterface():
+    def draw_entry_page():
+        
