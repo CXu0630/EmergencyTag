d4c24907baa3d30be91f7bc0221f600e8029ee66
CXu0607 <gx7@rice.edu>
2024-09-21 23:59:38 -0500

    removed a few comments

--- src/EmTag.py
+++ src/EmTag.py
@@ -6,17 +6,6 @@ from PyQt5.QtWidgets import QApplication
 
 from NfcWriter import NfcWriter
 
-# def write_on_tap(connection):
-#     temp_writer = NfcWriter(connection)
-    
-#     temp_writer.write_nonce()
-#     temp_writer.write_category("name", "Chloe X")
-#     temp_writer.write_category("blood_type", "O")
-#     temp_writer.write_category("em_contact", "1231231234")
-#     temp_writer.write_category("birth_date", "20000630")
-#     temp_writer.write_category("allergies", "mango")
-#     temp_writer.write_category("med_history", "diabetes")
-
 def main():
     nfcConnecter = NfcConnecter()
     nfcConnecter.start()
